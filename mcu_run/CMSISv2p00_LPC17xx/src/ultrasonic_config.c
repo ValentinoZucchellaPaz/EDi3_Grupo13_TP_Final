364a55f255b56c16a1bd2599039ca76120f7c59b
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-06-04 13:56:16 -0300

    feat: cambios de pines y espacio mcu

--- mcu_run/CMSISv2p00_LPC17xx/src/ultrasonic_config.c
+++ mcu_run/CMSISv2p00_LPC17xx/src/ultrasonic_config.c
@@ -1,5 +1,5 @@
-#include "ultrasonic_config.h"
 #include "LPC17xx.h"
+#include "ultrasonic_config.h"
 #include "lpc17xx_timer.h"
 #include "lpc17xx_gpio.h"
 
@@ -8,20 +8,20 @@
 // ---------------------------------------------------------------------------------------
 
 /**
- * @brief Configura pulso para que sensor comience una muestra:
+ * Configura pulso para que sensor comience una muestra:
  *
- * Configura el pin P1.0 como GPIO output
+ * Configura el pin P1.0 como GPIO
  * Configura TIMER2 va a hacer un PWM de periodo 60ms con ese pin y le da start.
  * Conectar a pin TRIG del sensor para que comience una muestra.
  */
-static void config_timer1_pwm(void)
+static void config_timer2_pwm(void)
 {
     // PWM periodo 60ms y high_time 10us (datasheet de HC-SR04)
     // se hace toggle de pin P1.0 en la ISR
 
     // config pin P1.0 como salida gpio
-    GPIO_SetDir(PORT_0, 1 << 18, GPIO_OUTPUT);
-    GPIO_SetPinState(PORT_0, PIN_18, DISABLE);
+    GPIO_SetDir(PORT_1, 1 << 0, GPIO_OUTPUT);
+    GPIO_SetPinState(PORT_1, PIN_0, DISABLE);
 
     // conf timer:
     // match 0 cuenta high_time e interrumpe, match 1 es el periodo (reset e interrumpe)
@@ -46,28 +46,28 @@ static void config_timer1_pwm(void)
     match1cfg.matchValue = 60000; // 60ms
 
     // reset de timer
-    TIM_InitTimer(LPC_TIM1, &tim);
-    TIM_Disable(LPC_TIM1);
-    TIM_ResetCounter(LPC_TIM1);
+    TIM_InitTimer(LPC_TIM2, &tim);
+    TIM_Disable(LPC_TIM2);
+    TIM_ResetCounter(LPC_TIM2);
     // config y prendo
-    TIM_ConfigMatch(LPC_TIM1, &match1cfg);
-    TIM_ConfigMatch(LPC_TIM1, &match0cfg);
+    TIM_ConfigMatch(LPC_TIM2, &match1cfg);
+    TIM_ConfigMatch(LPC_TIM2, &match0cfg);
     NVIC_EnableIRQ(TIMER2_IRQn);
-    TIM_Enable(LPC_TIM1);
+    TIM_Enable(LPC_TIM2);
 }
 
 // pwm isr handler
-void TIMER1_IRQHandler()
+void TIMER2_IRQHandler()
 {
-    if (TIM_GetIntStatus(LPC_TIM1, TIM_MR0_INT) == SET) // high time termino, pongo 0
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR0_INT) == SET) // high time termino, pongo 0
     {
-        TIM_ClearIntPending(LPC_TIM1, TIM_MR0_INT);
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR0_INT);
         GPIO_SetPinState(PORT_1, PIN_0, 0);
     }
 
-    if (TIM_GetIntStatus(LPC_TIM1, TIM_MR1_INT) == SET) // se cumplio periodo, pongo 1
+    if (TIM_GetIntStatus(LPC_TIM2, TIM_MR1_INT) == SET) // se cumplio periodo, pongo 1
     {
-        TIM_ClearIntPending(LPC_TIM1, TIM_MR1_INT);
+        TIM_ClearIntPending(LPC_TIM2, TIM_MR1_INT);
         GPIO_SetPinState(PORT_1, PIN_0, 1);
     }
 }
@@ -76,17 +76,18 @@ void TIMER1_IRQHandler()
 // CONFIGURO CAPTURE PARA PIN ECHO DEL SENSOR
 // ---------------------------------------------------------------------------------------
 
+static volatile uint32_t pwm_dc_prop[10];
 static volatile uint32_t start_pulse = 0; // tiempo de rising edge de pwm
 static volatile uint32_t end_pulse = 0;   // tiempo de falling edge de pwm
 static volatile uint32_t high_time = 0;   // end_pulse - start_pulse
 static volatile uint32_t distance_cm = 0; // distancia en cm
-static volatile uint32_t pin_status = 0;  // estado de pin
+static volatile uint32_t pin_status = 0;
 
 /**
  * @brief Configura el timer1 para que aumente la cuenta cada 1us y configura el CAP1.0 (P1.18) capture e interrumpa en flanco asc y desc
  * Luego si la distancia es menor a 20cm la ISR prende o apaga led P0.22
  */
-static void config_timer2_capture(void)
+static void config_timer1_capture(void)
 {
     // config pin P0.22 como salida gpio: cuando la distancia pase un umbral prende el led, cuando no apaga
     GPIO_SetDir(PORT_0, 1 << 22, GPIO_OUTPUT);
@@ -104,36 +105,36 @@ static void config_timer2_capture(void)
     cap_cfg.fallingEn = ENABLE;
     cap_cfg.intEn = ENABLE;
 
-    TIM_InitTimer(LPC_TIM2, &timer_cfg);
+    TIM_InitTimer(LPC_TIM1, &timer_cfg);
     // reset de timer
-    TIM_Disable(LPC_TIM2);
-    TIM_ResetCounter(LPC_TIM2);
+    TIM_Disable(LPC_TIM1);
+    TIM_ResetCounter(LPC_TIM1);
 
     // config y enciendo
-    TIM_PinConfig(TIM_CAP2_0_P0_4);
-    TIM_ConfigCapture(LPC_TIM2, &cap_cfg);
-    NVIC_EnableIRQ(TIMER2_IRQn);
-    TIM_Enable(LPC_TIM2);
+    TIM_PinConfig(TIM_CAP1_0_P1_18);
+    TIM_ConfigCapture(LPC_TIM1, &cap_cfg);
+    NVIC_EnableIRQ(TIMER1_IRQn);
+    TIM_Enable(LPC_TIM1);
 }
 
 /**
  * @brief Cuando viene una interrupcion de timer (capture), veo si fue rising o falling (segun el estado del pin), mido high-time y saco distancia.
  * Finalmente prendo o apago led segund distancia
  */
-void TIMER2_IRQHandler(void)
+void TIMER1_IRQHandler(void)
 {
-    if (TIM_GetIntStatus(LPC_TIM2, TIM_CR0_INT) == SET)
+    if (TIM_GetIntStatus(LPC_TIM1, TIM_CR0_INT) == SET)
     {
-        TIM_ClearIntPending(LPC_TIM2, TIM_CR0_INT);
-        pin_status = (GPIO_ReadValue(PORT_0) & (1 << 4)) >> 4;
+        TIM_ClearIntPending(LPC_TIM1, TIM_CR0_INT);
+        pin_status = (GPIO_ReadValue(PORT_1) & (1 << 18)) >> 18;
 
         if (pin_status == 1) // rising edge
         {
-            start_pulse = TIM_GetCaptureValue(LPC_TIM2, TIM_CAPTURE_0);
+            start_pulse = TIM_GetCaptureValue(LPC_TIM1, TIM_CAPTURE_0);
         }
         else // falling edge
         {
-            end_pulse = TIM_GetCaptureValue(LPC_TIM2, TIM_CAPTURE_0);
+            end_pulse = TIM_GetCaptureValue(LPC_TIM1, TIM_CAPTURE_0);
 
             // manejo ovf
             if (end_pulse < start_pulse)
@@ -155,27 +156,12 @@ void TIMER2_IRQHandler(void)
 // HAGO FUNCIONES PUBLICAS
 // ---------------------------------------------------------------------------------------
 
-/**
- * @brief
- * #### Configurar salida de placa (P1.0) a pin TRIG de sensor
- *
- * P1.0 es GPIO output que hace PWM 60ms periodo y 10us hightime (Timer2 con interrupciones), se siguen las indicaciones del datasheet del sensor)
- * Configura pin
- *
- * #### Configurar pin ECHO de sensor a entrada de placa (P1.18)
- *
- * P1.18 es CAP1.0 (Timer1), dispara interrupcion en ambos flanco, saca distancia con tiempo pasado y guarda en variable static
- * Además si la distancia es menor a 20cm prende el led rojo de la placa (P0.22) sino apaga
- */
 void Ultrasonic_Init(void)
 {
-    config_timer1_pwm();
-    config_timer2_capture();
+    config_timer2_pwm();
+    config_timer1_capture();
 }
 
-/**
- * @return distancia_cm
- */
 int Ultrasonic_GetDistance(void)
 {
     return distance_cm;
--- src/config/ultrasonic_config.h
+++ src/config/ultrasonic_config.h
@@ -1,8 +1,8 @@
 #ifndef ULTRASONIC_CONFIG_H
 #define ULTRASONIC_CONFIG_H
 
-static void config_timer2_pwm(void);
-static void config_timer1_capture(void);
+static void config_timer1_pwm(void);
+static void config_timer2_capture(void);
 void Ultrasonic_Init(void);
 int Ultrasonic_GetDistance(void);
 
